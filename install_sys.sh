3c28effb114c78c7e75cd9a87716cefcebeee17c
Michael Weitner <michael@weitnerembeddedsystem.com>
2022-04-07 10:26:49 +0200

    fix swap partition space dialog

--- install_sys.sh
+++ install_sys.sh
@@ -28,20 +28,19 @@ dialog --title "Choose your hard drive" --no-cancel --radiolist \
   15 60 4 "${devices_list[@]}" 2> hd
 
 hd=$(cat hd) && rm hd
-#echo $hd
 
 ################
 # Partitioning #
 ################
 
 default_size="8"
-dialog --no-cancel --inputbox
+dialog --no-cancel --inputbox \
   "You need three partitions: Boot, Root and Swap \n\
   The boot partition will be 512M \n\
   The root partition will be the remaining of the hard disk \n\n\
   Enter below the partition size (in Gb) for the Swap. \n\n\
   If you don't enter anything, it will default to ${default_size}G. \n" \
-  20 60 2 2> swap_size
+  20 60 2> swap_size
 size=$(cat swap_size) && rm swap_size
 
 [[ $size =~ ^[0-9]+$ ]] || size=$default_size
@@ -55,7 +54,6 @@ dialog --no-cancel \
   3 "No need - my hard disk is empty" 2> eraser
 
 hderaser=$(cat eraser); rm eraser
-echo $hderaser
 
 function erase_disk() {
   case $1 in
